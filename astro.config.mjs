dc68644509533884f78d29d86ddeb98148b66732
Saamiyah Peerun <swaniyah@gmail.com>
2023-08-07 22:20:13 +0400

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import pandacss from "@pandacss/dev/astro";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [pandacss(), react()],
   output: "server",
+  adapter: netlify()
 });
